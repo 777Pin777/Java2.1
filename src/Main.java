886dac169306f25e1e35a49b5304b0387a17a2f9
Kalinina Mariya <kalinina.mariya777@gmail.com>
2022-05-14 13:40:01 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountTickets = 2100;
+        int milesProgram = 20;
+        int bonus = amountTickets / milesProgram;
+        System.out.println("Your bonus is: " + bonus + " miles");
+
+    }
+}
